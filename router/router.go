a8e5616942a3299e7e676b213422464613334223
Chandan Acharya <chandan.acharya@vanillasteel.com>
2022-10-31 00:09:14 +0100

    removed bug creating server multiple times

--- router/router.go
+++ router/router.go
@@ -1,10 +1,8 @@
 package router
 
 import (
-	"fmt"
 	"github.com/chandanacharya1/customer-matching/middleware"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 // Router is exported and used in main.go
@@ -18,7 +16,5 @@ func Router() *mux.Router {
 	router.Handle("/request", middleware.Validate(middleware.CustomerRequest)).Methods("POST", "OPTIONS")
 	router.Handle("/partner", middleware.Validate(middleware.ListPartners)).Methods("GET", "OPTIONS")
 	router.Handle("/getpartner", middleware.Validate(middleware.GetPartner)).Methods("GET")
-	fmt.Println("Starting web server on port 8080")
-	http.ListenAndServe(":8080", router)
 	return router
 }
